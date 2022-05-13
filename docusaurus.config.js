573881fb27822f34ec0be539f97f0b2d46f8b88f
Jefferson Osagie Iyobosa <iyobosajefferson2@gmail.com>
2022-05-13 01:21:15 +0100

    chore: tailwind installation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,13 +6,12 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Lazerpay Documentation',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/logo.svg',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -34,18 +33,10 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/LazerPay-Finance/Lazerpay-documention-v2',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -57,9 +48,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: '',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Lazerpay logo',
           src: 'img/logo.svg',
         },
         items: [
@@ -67,11 +58,25 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            to: '/quick-links',
+            label: 'Quick Start',
+            position: 'right',
+          },
+          {
+            to: '/quick-links',
+            label: 'APIs',
+            position: 'right',
+          },
+          {
+            to: '/quick-links',
+            label: 'Support',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/LazerPay-Finance/Lazerpay-documention-v2',
             label: 'GitHub',
             position: 'right',
           },
@@ -84,7 +89,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Docs',
                 to: '/docs/intro',
               },
             ],
@@ -110,10 +115,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
               },
@@ -127,6 +128,9 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+    plugins: [
+      'docusaurus-tailwindcss'
+    ]
 };
 
 module.exports = config;
