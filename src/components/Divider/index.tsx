e1370266447f0b48119d1c26895e7f7127353f15
Jefferson Osagie Iyobosa <iyobosajefferson2@gmail.com>
2022-05-25 01:20:06 +0100

    chore: cleanup delete files

--- src/components/Divider/index.tsx
+++
@@ -1,7 +0,0 @@
-import classNames from 'classnames';
-
-const Divider = ({ className }: { className?: string })=> (
-  <div className={classNames(className, 'h-1 border-t py-2 border-neu-200')} />
-);
-
-export default Divider;
