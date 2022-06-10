db37df8ccdb0fb004cf2ee26845416e1e398fdaa
asapconet <aaronrickymeek@gmail.com>
2022-06-10 16:00:19 +0100

    Improvement: Payments Get started Cad cleanup

---
+++ components/GetstartedCard/data.ts
@@ -0,0 +1,15 @@
+export interface CardWithImage {
+  title: string
+  desc: string
+}
+
+export const cardWithImageData: CardWithImage[] = [
+  {
+    title: 'Accept Payments',
+    desc: 'Let your customers pay using our support payment system',
+  },
+  {
+    title: 'Verify payments',
+    desc: 'Automatically verify trasactions after payments using lazerpay verify API ',
+  },
+]
--- components/GetstartedCard/index.tsx
+++ components/GetstartedCard/index.tsx
@@ -2,11 +2,12 @@ import classNames from 'classnames'
 import { FaLink } from 'react-icons/fa'
 import { CgArrowLongRight } from 'react-icons/cg'
 import Styles from './index.module.scss'
+import { cardWithImageData } from './data'
 
 export const LinkedCard = ({ desc, title }) => {
   return (
     <div className='my-2 '>
-      <div className='bg-white text-sm mb-2 p-6 md:pt-9 shadow-lg rounded-8 lg:rounded-8 lg:w-92'>
+      <div className='bg-white text-sm mb-2 p-6 md:pt-9 drop-shadow-xs rounded-8 lg:rounded-8 lg:w-92'>
         <FaLink />
         <h5 className='text-pri-500 py-2 md:py-4 md:text-md font-bold'>
           {title}
@@ -25,17 +26,12 @@ export default function GetStarted(): JSX.Element {
     <section
       className={classNames(
         Styles.GetStarted,
-        'grid grid-cols-2 lg:flex-row w-full lg:space-y-1 w-full lg:space-x-6',
+        'grid md:grid-cols-2 lg:flex-row w-full lg:space-y-1 w-full lg:space-x-6',
       )}
     >
-      <LinkedCard
-        title='Accept Payments'
-        desc='Let your customers pay using our support payment system'
-      />
-      <LinkedCard
-        title='Verify payments'
-        desc='Automatically verify trasactions after payments using lazerpay verify API '
-      />
+      {cardWithImageData.map(({ title, desc }) => {
+        return <LinkedCard title={title} desc={desc} />
+      })}
     </section>
   )
 }
