f71a9dc065de19559058daedbd039936b590fff6
asapconet <aaronrickymeek@gmail.com>
2022-06-10 23:17:01 +0100

    Update: imported icons from figma and exchanged with FaIcons

--- components/GetstartedCard/index.tsx
+++ components/GetstartedCard/index.tsx
@@ -1,6 +1,4 @@
 import classNames from 'classnames'
-import { FaLink } from 'react-icons/fa'
-import { CgArrowLongRight } from 'react-icons/cg'
 import Styles from './index.module.scss'
 import {
   completeYourIntegrationData,
@@ -8,39 +6,45 @@ import {
   testAndGoData,
 } from './data'
 import LzLink from 'components/UI/Link'
+import LinkIcon from 'public/icons/link-icon'
+import ArrowRightIcon from 'public/icons/arrow-right-icon'
 
 // card with Icon
 export const LinkedCard = ({ desc, title, route }) => {
   return (
-    <LzLink className='my-2' to={route}>
-      <div className='bg-white text-sm mb-2 p-6 md:pt-9 drop-shadow-xs rounded-8 lg:rounded-8 lg:w-92'>
-        <FaLink />
-        <h5 className='text-pri-500 py-2 md:py-4 md:text-md font-bold'>
-          {title}
-        </h5>
-        <p className=' paragaph-1 md:paragraph-2'>{desc} </p>
-        <span className='pt-3 flex text-xl justify-end'>
-          <CgArrowLongRight />
-        </span>
-      </div>
-    </LzLink>
+    <>
+      <LzLink className='my-2' to={route}>
+        <div className='bg-white text-sm mb-2 p-6 md:pt-9 drop-shadow-xs rounded-8 lg:rounded-8 lg:w-92'>
+          <LinkIcon />
+          <h5 className='text-pri-500 py-2 md:py-4 paragraph-2 md:text-md font-bold'>
+            {title}
+          </h5>
+          <p className=' paragaph-1'>{desc} </p>
+          <span className='pt-3 flex text-xl justify-end'>
+            <ArrowRightIcon />
+          </span>
+        </div>
+      </LzLink>
+    </>
   )
 }
 
 // card without Icon
 export const PlainCard = ({ desc, title, route }) => {
   return (
-    <LzLink className='my-2 h-92 ' to={route}>
-      <div className='bg-white text-sm mb-2 px-5 md:pt-4 drop-shadow-xs rounded-8 lg:rounded-8 lg:w-92'>
-        <h5 className='text-pri-500 py-2 md:py-4 md:text-md font-bold'>
-          {title}
-        </h5>
-        <p className='h-12 paragraph-1 md:paragraph-2'>{desc} </p>
-        <span className='py-5 pb-5 flex text-xl justify-end'>
-          <CgArrowLongRight />
-        </span>
-      </div>
-    </LzLink>
+    <>
+      <LzLink className='my-2 h-92 ' to={route}>
+        <div className='bg-white text-sm mb-2 px-5 md:pt-4 drop-shadow-xs rounded-8 lg:rounded-8 lg:w-92'>
+          <h5 className='text-pri-500 py-2 md:py-4 paragraph-2 md:text-md font-bold'>
+            {title}
+          </h5>
+          <p className='h-12 paragraph-1'>{desc} </p>
+          <span className='py-5 pb-5 flex text-xl justify-end'>
+            <ArrowRightIcon />
+          </span>
+        </div>
+      </LzLink>
+    </>
   )
 }
 
@@ -56,7 +60,7 @@ export default function GetStarted(): JSX.Element {
       )}
     >
       {startIntegratingData.map(({ title, desc, route }) => {
-        return <LinkedCard title={title} desc={desc} route={route}/>
+        return <LinkedCard title={title} desc={desc} route={route} />
       })}
     </section>
   )
@@ -78,7 +82,7 @@ export function CompleteIntegration(): JSX.Element {
   )
 }
 
-// 3 Test and Go Live 
+// 3 Test and Go Live
 export function TestAndGoLive(): JSX.Element {
   return (
     <section
@@ -87,7 +91,7 @@ export function TestAndGoLive(): JSX.Element {
         'grid md:grid-cols-2 lg:flex-row w-full lg:space-y-1 w-full lg:gap-x-6',
       )}
     >
-      {testAndGoData.map(({ title, desc,route }) => {
+      {testAndGoData.map(({ title, desc, route }) => {
         return <PlainCard title={title} desc={desc} route={route} />
       })}
     </section>
