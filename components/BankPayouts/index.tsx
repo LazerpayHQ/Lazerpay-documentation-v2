eea4bed56ea447e85439e49f0811df3d3960b68a
asapconet <aaronrickymeek@gmail.com>
2022-07-03 04:20:58 +0100

    feat: mobile responsiveness [UI] for bank payouts page x9

--- components/BankPayouts/index.tsx
+++ components/BankPayouts/index.tsx
@@ -10,9 +10,11 @@ import { FaAngleRight } from 'react-icons/fa'
 const BankPayouts = () => {
   return (
     <>
-      <div className='grid grid-cols-2'>
-        <div className='col-span-1 flex flex-col justify-center text-left  pr-24 gap-y-4'>
-          <h1 className='heading-2-b'>Get Notified When Bank Payout is Live</h1>
+      <div className='grid md:grid-cols-2 space-y-12 sticky'>
+        <div className='col-span-1 flex flex-col justify-center text-left md:pr-24 space-y-8'>
+          <h1 className='heading-3-b lg:heading-2-b'>
+            Get Notified When Bank Payout is Live
+          </h1>
           <p className='paragraph-2'>
             Bank Payout to multiple countries in USD and other local currencies
             is coming soon. Reach out to{' '}
@@ -20,23 +22,33 @@ const BankPayouts = () => {
             questions regarding this.
           </p>
           <form className='flex flex-col space-y-6'>
-            <label className='font-proxima -mb-4'>Email</label>
+            <label className='font-proxima -mb-4 paragraph-1-s'>Email</label>
             <input
               type='email'
               id='email'
               className={classNames(
                 Styles.input,
-                'placeholder:px-5 placeholder:font-proxima',
+                'placeholder:px-5  placeholder:font-proxima',
               )}
               placeholder='Enter your email address'
             />
-            <LzButton variant='pri' rightIcon className='w-[412px]' size='lg'>
+            <LzButton
+              variant='pri'
+              rightIcon
+              className='w-[335px] lg:w-[412px]'
+              size='lg'
+            >
               Notify me
             </LzButton>
           </form>
         </div>
         <div className='col-span-1'>
-          <Image src={HandWithPenny} alt='' height={'740px'} />
+          <Image
+            src={HandWithPenny}
+            alt='pennyhandIcon'
+            height={'740px'}
+            className='w-[335px] h-[392px]'
+          />
         </div>
       </div>
     </>
