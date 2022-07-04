8b0e39092b7725feef9424e9c36680e466212c94
asapconet <aaronrickymeek@gmail.com>
2022-07-04 17:24:55 +0100

    chore: code checks & cleanup

--- components/BankPayouts/index.tsx
+++ components/BankPayouts/index.tsx
@@ -6,51 +6,73 @@ import LzButton from 'components/UI/button'
 import Styles from './index.module.scss'
 import classNames from 'classnames'
 import { FaAngleRight } from 'react-icons/fa'
+import { periphery } from './data'
+
+interface MyProps {
+  title: string
+  desc: string
+  label: string
+  type?: string
+  id?: string
+}
+
+export const PayoutLHS = ({ title, desc, label, id, type }: MyProps) => {
+  return (
+    <>
+      <div className='col-span-1 flex flex-col justify-center text-left md:pr-24 space-y-8'>
+        <h1 className='heading-3-b lg:heading-2-b'>{title}</h1>
+        <p className='paragraph-2'>{desc}</p>
+        <form className='flex flex-col space-y-6'>
+          <label className='font-proxima -mb-4 capitalize paragraph-1-s'>
+            {label}
+          </label>
+          <input
+            type={type}
+            id={id}
+            className={classNames(
+              Styles.input,
+              'placeholder:px-5  placeholder:font-proxima',
+            )}
+            placeholder='Enter your email address'
+          />
+          <LzButton
+            variant='pri'
+            rightIcon={<FaAngleRight />}
+            className='w-[335px] lg:w-[412px]'
+            size='lg'
+          >
+            Notify me
+          </LzButton>
+        </form>
+      </div>
+    </>
+  )
+}
 
 const BankPayouts = () => {
   return (
     <>
-      <div className='grid md:grid-cols-2 space-y-12 sticky'>
-        <div className='col-span-1 flex flex-col justify-center text-left md:pr-24 space-y-8'>
-          <h1 className='heading-3-b lg:heading-2-b'>
-            Get Notified When Bank Payout is Live
-          </h1>
-          <p className='paragraph-2'>
-            Bank Payout to multiple countries in USD and other local currencies
-            is coming soon. Reach out to{' '}
-            <LzLink>developers@lazerpay.finance</LzLink> if you have any
-            questions regarding this.
-          </p>
-          <form className='flex flex-col space-y-6'>
-            <label className='font-proxima -mb-4 paragraph-1-s'>Email</label>
-            <input
-              type='email'
-              id='email'
-              className={classNames(
-                Styles.input,
-                'placeholder:px-5  placeholder:font-proxima',
-              )}
-              placeholder='Enter your email address'
+      <section className='grid md:grid-cols-2 md:space-y-0 space-y-12 sticky'>
+        {periphery.map(({ id, type, label, title, desc }: MyProps) => {
+          return (
+            <PayoutLHS
+              id={id}
+              type={type}
+              label={label}
+              title={title}
+              desc={desc}
             />
-            <LzButton
-              variant='pri'
-              rightIcon
-              className='w-[335px] lg:w-[412px]'
-              size='lg'
-            >
-              Notify me
-            </LzButton>
-          </form>
-        </div>
+          )
+        })}
         <div className='col-span-1'>
           <Image
             src={HandWithPenny}
             alt='pennyhandIcon'
-            height={'740px'}
-            className='w-[335px] h-[392px]'
+            height={'847px'}
+            className='w-[412px] h-[392px]'
           />
         </div>
-      </div>
+      </section>
     </>
   )
 }
