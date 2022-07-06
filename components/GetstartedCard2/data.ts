7147d448778c9b3ca568a3b26b9d308d9c5ce3cd
Khadeeejah <wuraola81@gmail.com>
2022-07-06 19:26:43 +0100

    transfer-page

--- components/GetstartedCard2/data.ts
+++ components/GetstartedCard2/data.ts
@@ -17,33 +17,3 @@ export const startIntegratingData: CardWithImage[] = [
     },
 ]
 
-export const completeYourIntegrationData: CardWithImage[] = [
-    {
-        title: 'Payments Link',
-        desc: 'With payments Link you can share your unique payments link to anyone in the world.',
-        route: 'payments-link',
-    },
-    {
-        title: 'Partial Payments',
-        desc: 'Accept partial payments from your customers on your platform',
-        route: 'partial-payments',
-    },
-    {
-        title: 'Setup Webhooks',
-        desc: 'Listen to webhook even whenever certain action occurs.',
-        route: 'setup-webhooks',
-    },
-]
-
-export const testAndGoData: CardWithImage[] = [
-    {
-        title: 'Make Test Payments',
-        desc: "We are goin to describe how you can go about testing Lazerpay's integration",
-        route: 'make-test-payments',
-    },
-    {
-        title: 'Go Live',
-        desc: 'Learnn about the steps required to make your integration live, and start receiving crypto payments in minutes.',
-        route: 'go-live',
-    },
-]
