004e6e99e6eace21ce16b104efbde4e863f7b750
asapconet <aaronrickymeek@gmail.com>
2022-07-06 01:02:28 +0100

    chore: page [routes] setup

--- sidebar.config.tsx
+++ sidebar.config.tsx
@@ -45,7 +45,7 @@ const sidebar: SidebarItem[] = [
     label: 'Transfers',
     icon: <TransferIcon />,
     id: "transfers",
-    children: [],
+    children: [{ type: "docs", label: "Bank Payouts", id: "bank-transfers" }],
   },
   {
     type: 'docs',
