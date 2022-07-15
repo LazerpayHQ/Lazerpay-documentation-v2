9cf6c22c5a076dbba32887e1653ac69350427ccb
asapconet <aaronrickymeek@gmail.com>
2022-07-15 16:31:28 +0100

    Feature: form validation for email in transfer/bank-payouts x10

--- components/BankPayouts/PayoutLSH.tsx
+++ components/BankPayouts/PayoutLSH.tsx
@@ -1,13 +1,47 @@
 import LzInput from '../UI/input'
 import LzButton from 'components/UI/button'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+interface IFields {
+    email: string
+}
+
+const schema = yup
+    .object({
+        email: yup.string().email("Please input a valid email").required('Email is required'),
+    })
+    .required()
 
 export const PayoutLHS = ({ title, desc, label, type }) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IFields>({
+        resolver: yupResolver(schema),
+    })
+
+    const onSubmit = (values: IFields) => null
+
     return (
         <>
             <h1 className='heading-3-b lg:heading-2-b'>{title}</h1>
             <p className='paragraph-2' dangerouslySetInnerHTML={{ __html: desc }} />
-            <form className='flex flex-col space-y-6'>
-                <LzInput type={type} label={label} className='w-[335px] lg:w-[412px]' placeholder='Please enter your email' />
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className='flex flex-col space-y-6'
+            >
+                <LzInput
+                    type={type}
+                    label={label}
+                    {...register('email')}
+                    errors={errors.email?.message}
+                    className='w-[335px] lg:w-[412px]'
+                    placeholder='Please enter your email'
+                />
+
                 <LzButton
                     size='lg'
                     variant='pri'
