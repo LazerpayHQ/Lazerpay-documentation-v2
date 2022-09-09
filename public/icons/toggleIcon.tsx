72d33960bca11b3c891fb1dbac2dbf405a6b84fe
asapconet <aaronrickymeek@gmail.com>
2022-09-09 10:49:19 +0100

    bug: breake point fix

--- public/icons/toggleIcon.tsx
+++ public/icons/toggleIcon.tsx
@@ -58,7 +58,7 @@ export const ToggleIcon = () => {
             filterUnits='userSpaceOnUse'
             colorInterpolationFilters='sRGB'
           >
-            <feFlood flood-opacity='0' result='BackgroundImageFix' />
+            <feFlood floodOpacity='0' result='BackgroundImageFix' />
             <feColorMatrix
               in='SourceAlpha'
               type='matrix'
