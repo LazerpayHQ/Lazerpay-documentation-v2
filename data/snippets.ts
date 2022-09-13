346279a949b61db608037a45e006262323d6a6fa
Gideon Nnalue <emekannalue@gmail.com>
2022-09-13 14:27:48 +0100

    code blocks data

--- data/snippets.ts
+++ data/snippets.ts
@@ -131,6 +131,47 @@ export const snippets = {
 
     --header 'x-api-key: YOUR_PUBLIC_KEY' \
     `,
+    verifyPaymentGetResponse200:
+        `{
+        "status": "success",
+        "statusCode": 200,
+        "message": "Verification successful",
+        "data": {
+            "id": "92924b81-11fd-418f-bc4f-3ddab7e79e6b",
+            "reference": "nGfVSuX3IK",
+            "senderAddress": "0x451dEFC27B45808078e875556AF06bCFdC697BA4",
+            "recipientAddress": "0xCfe4e688c47Af0689224da9B9bAB51B4dA38D11c",
+            "actualAmount": 0.51,
+            "amountPaid": 0.51,
+            "amountPaidFiat": 299.7984,
+            "fiatAmount": 300,
+            "amountReceived": 0.52,
+            "amountReceivedFiat": 305.6768,
+            "coin": "BUSD",
+            "currency": "NGN",
+            "hash": "0xc80d9fa8ba4b13c685ad12ffdeb2a6f803f7c3832f51cc0376e5ff9a74c6fd93",
+            "blockNumber": 16684797,
+            "type": "received",
+            "acceptPartialPayment": true,
+            "status": "confirmed",
+            "network": "mainnet",
+            "blockchain": "Binance Smart Chain",
+            "customer": {
+                "id": "1c4d885e-4058-45f0-8d74-ff79fe439e75",
+                "customerName": "Abdulfatai Suleiman",
+                "customerEmail": "abdulfataisuleiman67@gmail.com",
+                "customerPhone": null
+            }
+            "paymentLink": {},
+            "paymentButton": {},
+            "feeInCrypto": 0.01
+        }`,
+    verifyPaymentGetResponse404:
+        `{
+        "message": "Transaction not found",
+        "status": "error"
+        "statusCode": 404,
+    }`,
     verifyPaymentNodeSDK:
         `const LazerPay = require('lazerpay-node-sdk');
     const lazerpay = new LazerPay(LAZER_PUBLIC_KEY, LAZER_SECRET_KEY);
