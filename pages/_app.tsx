8327a68f5f6d97e8e665ca08a826cb013764a3f6
Gideon Nnalue <emekannalue@gmail.com>
2022-10-07 00:34:49 +0100

    fix toast issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,12 @@ import '../styles/prism.scss'
 import '../styles/globals.scss'
 import 'react-toastify/dist/ReactToastify.css'
 import siteConfig from 'site.config'
-import { ToastContainer } from 'react-toastify'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <DefaultSeo {...siteConfig.seo} />
       <Component {...pageProps} />
-      <ToastContainer hideProgressBar />
     </>
   )
 }
